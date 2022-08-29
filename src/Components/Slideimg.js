90cda09976c0495506aa1701a62d9e66a4141a1c
riya-rk250902 <nepriya25@gmail.com>
2022-08-29 02:37:18 -0500

    again a commit for front

--- src/Components/Slideimg.js
+++ src/Components/Slideimg.js
@@ -1,47 +1,10 @@
-import React, { useEffect, useState} from "react";
-import {AiOutlineVerticalRight, AiOutlineVerticalLeft} from "react-icons/ai";
-const featureProducts=[
-     "/assests/foodimg1.jpg",
-    "/assests/foodimg2.jpg",
-     "/assests/foodimg3.jpg",    
-];
-  let count = 0;
-export default function Slider() {
-    const [currentIndex,setCurrentIndex] = useState(0);
-    useEffect(() => {
-      startSlider()
-    }, [])
-
-    const startSlider = () => {
-        setInterval(() => {
-    handleonNextClick()
-        }, 3000);
-    }
-
-    const handleonNextClick = () => {
-    count =(count + 1) % featureProducts.length;
-    setCurrentIndex(count);
-    };
-    const handleonPrevClick = () => {
-     const productsLength = featureProducts.length;
-     count = (currentIndex + productsLength - 1) % productsLength;
-      setCurrentIndex(count);
-    };
-    return (
-    <div className="w-full select-none relative">
-    <div className="aspect-w-16 aspect-h-9">
-    <img src={featureProducts[0]
-    [currentIndex]} alt=""/>
+import React from 'react'
+import  "./slider.css" ;
+export default function Slideimg() {
+  return (
+    <div>
+    <div class="maindiv">
+       </div>
     </div>
-        
-        <div className="absolute top-1/2 transform -translate-y-1/2 px-3 justify-between items-center">
-        <button className="bg-black text-white p-1 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition"
-        onClick={handleonPrevClick}>
-            <AiOutlineVerticalRight size ={30}/></button> 
-        <button className="bg-black text-white p-1 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition"
-        onClick={handleonNextClick}>
-            <AiOutlineVerticalLeft size={30}/></button>
-        </div>
-    </div>
-    )
-    };
+  );
+};
